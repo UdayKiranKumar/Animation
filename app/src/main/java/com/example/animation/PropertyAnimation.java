a61644f4bb3d87d951ab30180cbf326dbabc12c1
Uday Kiran Kumar <udaykirankumar8329@gmail.com>
2019-11-19 09:06:49 +0530

    Add files via upload

--- app/src/main/java/com/example/animation/PropertyAnimation.java
+++ app/src/main/java/com/example/animation/PropertyAnimation.java
@@ -1,39 +1,63 @@
-package com.example.animation;
+package com.example.propertyanimation;
 
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.AnimationSet;
-
-import androidx.annotation.Nullable;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
 
 public class PropertyAnimation extends View {
-    Paint paint;
-    AnimationSet animationSet;
-    float x,y,radius;
     final int AnimationDuration = 4000;
     final int AnimationDelay = 1000;
-
+    final int ColorAdjust = 5;
+    float x,y,radius;
+    Paint paint;
+    AnimatorSet animatorSet;
     public PropertyAnimation(Context context) {
         super(context);
     }
-    void setRadius(float r)
-    {
-        radius = r;
-        paint.setColor(Color.GREEN + (int)radius );
-    }
-
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        super.onSizeChanged(w, h, oldw, oldh);
-        ObjectAnimator growAnimator = ObjectAnimator.ofFloat(this,"radius",getWidth() , 0);
-    }
-
-    public PropertyAnimation(Context context, @Nullable AttributeSet attrs) {
+    public PropertyAnimation(Context context, AttributeSet attrs){
         super(context, attrs);
+
+        paint = new Paint();
+        animatorSet = new AnimatorSet();
+    }
+    void setRadius(float r){
+        radius = r;
+        paint.setColor(Color.GREEN + (int) radius / ColorAdjust);
+        invalidate();
+    }
+    protected void onSizeChanged(int w, int h,int oldw, int oldh){
+        super.onSizeChanged(w,h,oldw,oldh);
+        ObjectAnimator growAnimator = ObjectAnimator.ofFloat(this,"radius",0,getWidth());
+        growAnimator.setDuration(AnimationDuration);
+        growAnimator.setInterpolator(new LinearInterpolator());
+        ObjectAnimator shrinkAnimator = ObjectAnimator.ofFloat(this,"radius",getWidth(),0);
+        shrinkAnimator.setDuration(AnimationDuration);
+        shrinkAnimator.setInterpolator(new DecelerateInterpolator());
+        shrinkAnimator.setStartDelay(AnimationDelay);
+        animatorSet.play(growAnimator).before(shrinkAnimator);
+    }
+    public boolean onTouchEvent(MotionEvent event){
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x = event.getX();
+                y = event.getY();
+                if (animatorSet != null && animatorSet.isRunning())
+                    animatorSet.cancel();
+                animatorSet.start();
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        canvas.drawCircle(x,y,radius,paint);
     }
 }
